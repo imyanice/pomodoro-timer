d98429bcc7302bcac7a2cfce9dc8ccdd82c55072
Yanice <git@yanice.me>
2024-05-01 20:04:02 +0200

    fixed config and added a break time

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,9 @@ pub struct Config {
 }
 
 fn main() -> Result<(), slint::PlatformError> {
+    let mut config = get_config();
+    config.enabled = false;
+    set_config(config);
     let ui = AppWindow::new()?;
     ui.on_start_timer({
         let ui_handle = ui.as_weak();
@@ -29,12 +32,10 @@ fn main() -> Result<(), slint::PlatformError> {
 
     let ui_handle = ui.as_weak();
     std::thread::spawn(move || {
-        let mut session_time = 60 * 25;
-        let mut break_time = 60 * 5;
+        let mut session_time = 10 ;
+        let mut break_time = 5 ;
         loop {
-
             if get_config().enabled {
-                println!("gi3");
                 let ui = ui_handle.clone();
 
                 if session_time > 0 {
@@ -43,11 +44,19 @@ fn main() -> Result<(), slint::PlatformError> {
                     println!("{:?}", time_seconds);
                     slint::invoke_from_event_loop(move || ui.unwrap().set_time((time_minutes.to_string() + ":" + &time_seconds.to_string()).into())).expect("TODO: panic message");
                     session_time -= 1;
-                } else if break_time > 0 {
-                } else if break_time == 0 {
-                    session_time = 60 * 25;
-                    break_time = 60 * 5;
-                }
+                } else if break_time > 1 {
+                    let time_minutes = break_time / 60;
+                    let time_seconds = break_time - time_minutes * 60;
+                    println!("{:?}", time_seconds);
+                    let ui2 = ui.clone();
+                    slint::invoke_from_event_loop(move || ui.unwrap().set_time((time_minutes.to_string() + ":" + &time_seconds.to_string()).into())).expect("TODO: panic message");
+                    break_time -= 1;
+                    if break_time == 1 {
+                        slint::invoke_from_event_loop(move || ui2.unwrap().set_time((time_minutes.to_string() + ":" + &time_seconds.to_string()).into())).expect("TODO: panic message");
+                        session_time = 60 * 25;
+                        break_time = 60 * 5;
+                    }
+                } 
                 thread::sleep(Duration::from_millis(1000));
             }
         }
@@ -58,8 +67,11 @@ fn main() -> Result<(), slint::PlatformError> {
 
 pub fn get_config() -> Config {
     let path = data_dir().join("config.toml");
-
-    confy::load_path(path).expect("Could not load config")
+    
+    confy::load_path(path).unwrap_or_else(|_| {
+        set_config(Config { enabled: false });
+        Config { enabled: false }
+    })
 }
 
 pub fn set_config(config: Config) {
